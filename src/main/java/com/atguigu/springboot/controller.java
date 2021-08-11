7ccec7e6238bb3e11e1f247fa7efe0f6fc9f1161
wangru <wangru1602@163.com>
2021-08-11 10:15:49 +0800

    wrbranch2-first-commit

--- src/main/java/com/atguigu/springboot/controller.java
+++ src/main/java/com/atguigu/springboot/controller.java
@@ -17,9 +17,8 @@ public class controller {
 //        System.out.println(path3);
         Arrays.fill(path3,'a');
         System.out.println(path3);
-        System.out.println("wrbranch first commit");
-        System.out.println("wrbranch second commit");
-        System.out.println("wrbranch third commit");
+        System.out.println("wrbranch2 first commit");
+
        return "hello world quick!";
 
 
